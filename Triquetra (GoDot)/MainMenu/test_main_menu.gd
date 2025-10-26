873813c8a29c2c5367a915ae71e49c76b470bdd9
Abdullah Shamam <abdullahdhibian@gmail.com>
2025-10-26 03:00:47 +0000

    fade transition

--- Triquetra (GoDot)/MainMenu/test_main_menu.gd
+++ Triquetra (GoDot)/MainMenu/test_main_menu.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+var button_type = null
+
 func _ready() -> void:
 	pass
 
@@ -8,8 +10,22 @@ func _process(delta: float):
 	
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Level_1.tscn")
+	button_type = "start"
+	$Fade_Transition.show()
+	$Fade_Transition/FadeTimer.start()
+	$Fade_Transition/AnimationPlayer.play("fade_in")
 
 
 func _on_quit_pressed() -> void:
-	get_tree().quit()
+	button_type = "quit"
+	$Fade_Transition.show()
+	$Fade_Transition/FadeTimer.start()
+	$Fade_Transition/AnimationPlayer.play("fade_out")
+
+
+func _on_fade_timer_timeout() -> void:
+	if button_type == "start":
+		get_tree().change_scene_to_file("res://Scenes/Level_1.tscn")
+	
+	elif button_type == "quit":
+		get_tree().quit()
